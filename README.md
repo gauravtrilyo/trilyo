d461c6c4bdb07fd2cb54f55b086970180c5620bb
priyansh <singhalpriyansh58@gmail.com>
2021-03-09 17:04:23 +0530

    Initial Commit + Monaco Editor

---
+++ README.md
@@ -0,0 +1,6 @@
+# Trilyo Internship
+
+#### Task 1: 
+
+[x] Added Monaco Editor in create react App 
+[x] Added Get and Post req to dummy api 
---
+++ monoco-editor/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Editor from '@monaco-editor/react'
+import React,{useEffect, useState} from "react"
+import axios from 'axios';
+
+function App() {
+  const [items, setItems] = useState([]);
+  
+  useEffect(()=>{
+    const fetchItems= async()=>{
+      const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+      const requestData  =  await axios.get(API_URL);
+      setItems(requestData.data)
+    }
+    fetchItems();
+  }, [])
+  
+  console.log(items);
+
+  return (
+    <div className="App">
+      <h1 style={{backgroundColor:'purple',alignItems:'self-start'}}>Monaco Editor</h1>
+     <div className="editor" >
+       <Editor 
+          theme="vs-dark"
+          height="80vh"
+          width="70vw"
+          defaultLanguage = "javascript"
+          // defaultValue={items}
+          defaultValue={items.reduce((acc, item) => acc.concat(item.title).concat("\n"), "")}
+       />
+     </div>
+    </div>
+  );
+}
+
+export default App;
